709ce7a48a7707de5bc08c8da987f202ae10d335
Miguel Pernía <mperniaponce2@alumnos.cei.es>
2025-10-09 13:49:30 +0200

    index.js with notes added

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// Aunque en general trato de trabajar en inglés, dado el carácter de estas anotaciones, voy a usar mi idioma preferido
+
+//Importaciones necesarias para que funcione este programa
+
 import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
+
+//Esto es simplemente un mensaje formateado de explicación
 
 console.log(chalk.blue.bold('\n Bienvenido al Analizador de Recursos Gráficos(ARG) de Daemon Studios \n'));
 console.log('Este proyecto ha sido diseñado para ayudarte a analizar carpetas llenas de recursos gráficos.');
@@ -13,8 +19,11 @@ console.log('Los recursos que necesitan ser optimizados' + chalk.red.bold(' se m
 console.log(chalk.blue.bold('RECUERDA mantener tus recursos optimizados para mejorar el rendimiento de tu proyecto.\n'));
 console.log('¡Listo para comenzar! \n');
 
-
+//Declaramos una función asíncrona encargada de pedir la ruta a analizar por terminal
 const askDirectory = async() => {
+
+//Con el paquete inquirer podemos lanzar texto por terminal y recibir respuestas. 
+//Usamos await ya que es una operación asíncrona
     const answer = await inquirer.prompt([
         {
             type: 'input',
@@ -23,33 +32,37 @@ const askDirectory = async() => {
             default: './fotosDePrueba',
         }
     ]);
+// Devolvemos la ruta introducida, o la ruta por defecto si no se ha escrito nada.
     return answer.directoryPath;
 };
+//Con un bloque try, lanzamos otra o. asíncrona que analice el directorio
 const analyzeDirectory = async() => {
 
 try {
+// Llamamos a askdirectory y guardamos la ruta
         const directoryPath = await askDirectory();
 
-        // Verificar si el directorio existe
+// Verificamos si el directorio existe, y si no es así, mensajito formateado
         if (!fs.existsSync(directoryPath)) {
             console.log(chalk.red('El directorio no existe'));
             return;
         }
-
+//Aquí leemos el directorio usando fs
         const files = await fs.promises.readdir(directoryPath);
 
-        // Procesar archivos secuencialmente
+// Procesamos archivos secuencialmente con un bucle for
         for (const file of files) {
             const filePath = path.join(directoryPath, file);
-            
+//Otro try para obtener las stats (tamaño, formato, etc...)         
             try {
                 const stats = await fs.promises.stat(filePath);
-                
+//Verificamos que sea un archivo (no un directorio)       
                 if (stats.isFile()) {
+//Coloreamos, pasamos de bytes a kb y declaramos nuestra flechita
                     let colorFunc;
                     const sizeKB = stats.size / 1024;
                     let arrow = '';
-                    
+// Bucle de instrucciones if para separar en 3 categorias                    
                     if (sizeKB < 100) {
                         colorFunc = chalk.green;
                     } else if (sizeKB <= 250) {
@@ -58,17 +71,19 @@ try {
                         colorFunc = chalk.red;
                         arrow = chalk.red.bold('<---OPTIMIZAR---');
                     }
-                    
+//Pintamos la lista por colores                    
                     console.log(colorFunc(`${file}\t${stats.size} bytes`) + ('  ')+(arrow ? '' + arrow : ''));
                 }
+//Cerramos el bucle try con catch de errores, y ya que estoy, los he pintado de rojo tb
             } catch (err) {
                 console.error(chalk.red(`Error al leer: ${file}`), err);
             }
         }
+//Los de antes eran errores de archivos concreto, y ahora manejamos errores generales
     } catch (err) {
         console.error(chalk.red('Error al leer archivos:'), err);
     }
 };
 
-// Execute the analysis
+//Ejecutamos el análisis
 analyzeDirectory();
